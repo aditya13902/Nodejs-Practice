8eb98697fcc83922f0d2f4f477cf2bcad429f22b
adi <adityakumark139@gmail.com>
2021-10-19 21:49:57 +0530

    view engine

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require('express');
 const port=8000;
 const app = express();
 app.use('/',require('./routes'));
+
+// setup view engine
+app.set('view engine',ejs);
+app.set('views','./views');
 app.listen(port,(err)=>{
     if(err){
         console.log(`Error -- ${err}`);
